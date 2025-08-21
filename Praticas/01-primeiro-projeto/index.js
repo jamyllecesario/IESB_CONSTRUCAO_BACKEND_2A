07a4a8f025d51b1d4b44863e0d6eefde19ab9fee
jamyllecesario <jamyllecesario01@gmail.com>
2025-08-20 21:37:10 -0300

    projeto1

---
+++ Praticas/01-primeiro-projeto/index.js
@@ -0,0 +1,21 @@
+console.log("rodado meu primeiro projeto em NODE")
+
+let nome = "Jamylle"
+
+let sobreNome = "Cesario"
+
+console.log(nome + " " + sobreNome)
+
+let idade = 21
+
+console.log("A idade do " + nome + " é " + idade)
+
+let exercicio = 0.5
+
+let trabalho = 1.5
+
+let prova = 4.5
+
+let notaA1Calculada = exercicio + trabalho + prova
+
+console.log ("NOTA A1:" + notaA1Calculada )
